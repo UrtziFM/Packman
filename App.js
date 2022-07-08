d15f7923b7d915f343c105021d3e63e65952de4d
u100247 <u100247@MM0012B7.local>
2022-07-08 23:06:14 +0200

    Starting Packman game

---
+++ App.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.getElementById('score');
+    const width = 28 // 28 x 28 = 784 squares
+
+    //layout grid and what is in the squares
+    const layout = [
+        
+    ]
+})
