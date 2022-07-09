c101d4dadc5126f4a0dac8a2a0473981e0357803
u100247 <u100247@MM0012B7.local>
2022-07-09 13:23:06 +0200

    Pacman is moving but we have check how to banned doing it through walls

--- App.js
+++ App.js
@@ -3,8 +3,90 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.getElementById('score');
     const width = 28 // 28 x 28 = 784 squares
 
-    //layout grid and what is in the squares
+    //layout grid and what is in the squares 
+    // Legend: 0 - pac-dot, 1 - wall, 2 - ghostlair, 3 - power pellet, 4 - empty.
     const layout = [
-        
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
     ]
+
+    const squares = [] // is empty because we will be pushing into
+
+    // draw the grid and render it
+    function createBoard(){
+        for (let i=0; i<layout.length; i++){
+            const square = document.createElement("div");
+            grid.appendChild(square);
+            squares.push(square);
+            // add layout to the board
+            if(layout[i]===0){
+                squares[i].classList.add("pac-dot")
+            } else if (layout[i]===1){
+                squares[i].classList.add("wall")
+            } else if (layout[i]===3){
+                squares[i].classList.add("power-pellet")
+            }
+        }
+    }
+    // call the function
+    createBoard();
+
+    // starting position of pacman 
+    let pacmanCurrentIndex = 490;
+    squares[pacmanCurrentIndex].classList.add("pac-man");
+
+    // move pacman
+    function movePacman(e) {
+       squares[pacmanCurrentIndex].classList.remove("pac-man");
+       
+       switch (e.keyCode){
+           case 37:
+               if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1;
+                break;
+            case  38:
+                if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width;
+                break;
+            case  39:
+                if (pacmanCurrentIndex % width < width -1) pacmanCurrentIndex += 1;
+                break;
+            case  40:
+                if (pacmanCurrentIndex + width < width*width) pacmanCurrentIndex += width;
+                break;
+       }
+
+       squares[pacmanCurrentIndex].classList.add("pac-man");
+
+       //pacDotEaten()
+       //powerPelletEaten()
+       //checkForGameOver()
+       //checkForWin()
+    }
+    // add event to move pacman
+    document.addEventListener("keyup", movePacman);
 })
